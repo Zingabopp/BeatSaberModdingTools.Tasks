c536191f62b473e7cc15593a5a4ee003a65d6413
Zingabopp <Zingabopp@gmail.com>
2020-04-15 11:13:21 -0500

    Add IsProcessRunning task

---
+++ BSMTTasks/IsProcessRunning.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Build.Framework;
+using System;
+using System.Diagnostics;
+
+namespace BSMTTasks
+{
+    public class IsProcessRunning : Microsoft.Build.Utilities.Task
+    {
+        [Required]
+        public virtual string ProcessName { get; set; }
+        [Output]
+        public virtual bool IsRunning { get; set; }
+
+        public override bool Execute()
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(ProcessName))
+                {
+                    return false;
+                }
+                foreach (Process proc in Process.GetProcesses())
+                {
+                    if (proc.ProcessName.Contains(ProcessName))
+                    {
+                        IsRunning = true;
+                        break;
+                    }
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log.LogErrorFromException(ex);
+                return false;
+            }
+        }
+    }
+}
