582905be8d2cc6990c0f6a56af7249c326e6df59
Zingabopp <Zingabopp@gmail.com>
2020-06-20 22:36:07 -0500

    Fixed LogWrapper methods calling themselves

--- BeatSaberModdingTools.Tasks/Utilities/LogWrapper.cs
+++ BeatSaberModdingTools.Tasks/Utilities/LogWrapper.cs
@@ -26,16 +26,16 @@ namespace BeatSaberModdingTools.Tasks.Utilties
         public void LogError(string message, params object[] messageArgs) => Logger.LogError(message, messageArgs);
 
         /// <inheritdoc/>
-        public void LogErrorFromException(Exception exception) => LogErrorFromException(exception);
+        public void LogErrorFromException(Exception exception) => Logger.LogErrorFromException(exception);
 
         /// <inheritdoc/>
-        public void LogMessage(MessageImportance importance, string message, params object[] messageArgs) => LogMessage(importance, message, messageArgs);
+        public void LogMessage(MessageImportance importance, string message, params object[] messageArgs) => Logger.LogMessage(importance, message, messageArgs);
 
         /// <inheritdoc/>
         public void LogWarning(string subcategory, string warningCode, string helpKeyword, string file, int lineNumber, int columnNumber, int endLineNumber, int endColumnNumber, string message, params object[] messageArgs)
             => Logger.LogWarning(subcategory, warningCode, helpKeyword, file, lineNumber, columnNumber, endLineNumber, endColumnNumber, message, messageArgs);
 
         /// <inheritdoc/>
-        public void LogWarning(string message, params object[] messageArgs) => LogWarning(message, messageArgs);
+        public void LogWarning(string message, params object[] messageArgs) => Logger.LogWarning(message, messageArgs);
     }
 }
